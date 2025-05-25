9df3dff212ff139f180713e122fc3016287ad03e
UnHeat <anbi2682006@gmail.com>
2025-05-25 09:29:04 +0700

    finish

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
 const mylibrary =[];
 
+
 function Book(author, title, pages, read_status){
     this.author = author;
     this.title = title;
@@ -24,11 +25,37 @@ function display(){
         } )
     }       
 }
-addBook("sdsa","s","ji","sd");
-addBook("sdsa","s","ji","sd");
-addBook("sdsa","s","ji","sd");
-addBook("sdsa","s","ji","sd");
-addBook("sdsa","s","ji","sd");
+function display_new(){
+    const props = ["author","title","pages","read_status"];
+    const col =  [ ".author",".title",".pages",".read_status"];
+        props.forEach((prop,i) =>{
+            const cell = document.createElement("div");
+            cell.textContent = mylibrary[mylibrary.length-1][prop];
+            document.querySelector(col[i]).appendChild(cell);
+        })
+
+}
+function input(){
+    const dialog = document.querySelector('dialog');
+    let author_in = document.getElementById('author').value;
+    let title_in = document.getElementById('title').value;
+    let pages_in = document.getElementById('pages').value;
+    let read_in = document.getElementById('read').value;
+    addBook(author_in,title_in,pages_in,read_in);
+    document.getElementById('author').value ='';
+    document.getElementById('title').value = '';
+    document.getElementById('pages').value ='';
+    document.getElementById('read').value='';
+
+    display_new();
+    dialog.close();
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+    const dialog = document.querySelector('dialog');
+    document.querySelector('.submit').addEventListener("click", input);
+    document.querySelector(".addBook").addEventListener("click",()=>{
+    dialog.show();
+});
     display();
   });
